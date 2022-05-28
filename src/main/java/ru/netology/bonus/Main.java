b8cf911082bbca73878280508ddac7d64a75de61
Invalid2User <lapina.gen@gmail.com>
2022-05-28 13:52:52 +0300

    first

---
+++ src/main/java/ru/netology/bonus/Main.java
@@ -0,0 +1,10 @@
+package ru.netology.bonus;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long bonus = service.calculate(1000_60, true);
+
+        System.out.println(bonus);
+    }
+}
